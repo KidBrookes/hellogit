54784aa753974996a6f16435abd8183c0501ca0b
KidBrookes <username@users.noreply.github.com>
2019-02-07 15:08:01 -0400

    Added source file

--- hello.cpp
+++ hello.cpp
@@ -1,1 +1,29 @@
-hello.cpp
+#include <random>
+#include <iostream>
+using namespace std;
+
+int main()
+{
+random_device rd;
+mt19937 eng1 (rd ());
+mt19937 eng2 (1337);
+int min =10, max = 100;
+
+uniform_int_distribution<> unifrm(min, max);
+normal_distribution<float> norm(mean, sd);
+
+
+//generate 20 normally distributed floats
+for (int n = 0; n < 20; n ++)
+{
+	cout<<norm(eng1)<<" ";
+  }
+
+
+//generate 20 uniformly distributed integers
+for(int n = 0; n < 20 ; n+=)
+{
+	cout<<unifrm(eng2)<<" ";
+  }
+return 0;
+}
