061faf117acef66efb22b8269d8b8d68ec81a264
KidBrookes <username@users.noreply.github.com>
2019-03-05 22:48:45 -0400

    Added source file

--- Vectors.cpp
+++ Vectors.cpp
@@ -1,13 +1,17 @@
-#include <vector>
+#include <vector> //must include header for vectors
 #include <iostream>
+#include <string>
 using namespace std;
 
-
+//push_back() adds value to the end of the vector
+//pop_back()  removes the last value
 
 int main(){
 
-vector<int> v1 = {0, 1, 2, 3};
-v1.push_back(4);
+vector<int> v1 = {10, 11, 21, 31};
+vector <char> v;
+
+v1.push_back(41);
 cout << v1[4];
 
 
